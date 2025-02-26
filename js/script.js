4dcc35c833cddd395e617e5ffb4311535beb7bda
entrega0 <lfic1997@gmail.com>
2025-02-26 00:16:09 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -139,21 +139,21 @@ const productos = [
             "img/prendas/topinclinado_4.jpg"
         ]
     },
-    {
-        "id": 11,
-        "name": "Musculosa Raibow",
-        "description": "Largo: 40cm - Contorno : 80cm",
-        "cost": 1200,
-        "category": "Prenda",
-        "images": [
-            "img/prendas/toparcoiris.jpg",
-            "img/prendas/toparcoiris_1.jpg",
-            "img/prendas/toparcoiris_2.jpg",
-            "img/prendas/toparcoiris_3.jpg",
-            "img/prendas/toparcoiris_5.jpg",
-            "img/prendas/toparcoiris_6.jpg"
-        ]
-    },
+    // {
+    //     "id": 11,
+    //     "name": "Musculosa Raibow",
+    //     "description": "Largo: 40cm - Contorno : 80cm",
+    //     "cost": 1200,
+    //     "category": "Prenda",
+    //     "images": [
+    //         "img/prendas/toparcoiris.jpg",
+    //         "img/prendas/toparcoiris_1.jpg",
+    //         "img/prendas/toparcoiris_2.jpg",
+    //         "img/prendas/toparcoiris_3.jpg",
+    //         "img/prendas/toparcoiris_5.jpg",
+    //         "img/prendas/toparcoiris_6.jpg"
+    //     ]
+    // },
     {
         "id": 12,
         "name": "Top crop",
